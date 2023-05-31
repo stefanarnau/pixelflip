55720673f1c8f7934b7b8b7bb5e5e2ae55cd49b5
plkn <fischmechanik@gmail.com>
2023-05-31 17:21:09 +0200

    seminar stuff

---
+++ eeg_seminar_erp.m
@@ -0,0 +1,157 @@
+clear all;
+
+% PATH VARS
+PATH_EEGLAB      = '/home/plkn/eeglab2022.1/';
+PATH_AUTOCLEANED = '/mnt/data_dump/pixelflip/2_cleaned/';
+
+% Subject list
+subject_list = {'VP01', 'VP02', 'VP03', 'VP05', 'VP06', 'VP08', 'VP12', 'VP07', 'VP11', 'VP09', 'VP16', 'VP17'};
+
+% Init eeglab
+addpath(PATH_EEGLAB);
+eeglab;
+
+%% ========================= CALCULATE ERPs ======================================================================================================================
+
+% Load info
+EEG = pop_loadset('filename', [subject_list{1}, '_cleaned_cue_erp.set'], 'filepath', PATH_AUTOCLEANED, 'loadmode', 'info');
+
+% Get erp times
+erp_times = EEG.times(EEG.times >= -200 & EEG.times <= 1500);
+
+% Matrices to collect data. Dimensionality: subjects x channels x times
+erp_easy_accu = zeros(length(subject_list), EEG.nbchan, length(EEG.times));
+erp_easy_flip = zeros(length(subject_list), EEG.nbchan, length(EEG.times));
+erp_hard_accu = zeros(length(subject_list), EEG.nbchan, length(EEG.times));
+erp_hard_flip = zeros(length(subject_list), EEG.nbchan, length(EEG.times));
+
+% Loop subjects
+ids = [];
+for s = 1 : length(subject_list)
+
+    % Get subject id as string
+    subject = subject_list{s};
+
+    % Collect IDs as number
+    ids(s) = str2num(subject(3 : 4));
+
+    % Load subject data. EEG data has dimensionality channels x times x trials
+    EEG = pop_loadset('filename',    [subject, '_cleaned_cue_erp.set'], 'filepath', PATH_AUTOCLEANED, 'loadmode', 'all');
+    
+    % Get trial-indices of conditions
+    idx_easy_accu = EEG.trialinfo(:, 4) == 0 & EEG.trialinfo(:, 3) == 1;
+    idx_easy_flip = EEG.trialinfo(:, 4) == 0 & EEG.trialinfo(:, 3) == 0;
+    idx_hard_accu = EEG.trialinfo(:, 4) == 1 & EEG.trialinfo(:, 3) == 1;
+    idx_hard_flip = EEG.trialinfo(:, 4) == 1 & EEG.trialinfo(:, 3) == 0;
+    
+    % Calculate subject ERPs by averaging across trials for each condition.
+    erp_easy_accu(s, :, :) = mean(squeeze(EEG.data(:, :, idx_easy_accu)), 3);
+    erp_easy_flip(s, :, :) = mean(squeeze(EEG.data(:, :, idx_easy_flip)), 3);
+    erp_hard_accu(s, :, :) = mean(squeeze(EEG.data(:, :, idx_hard_accu)), 3);
+    erp_hard_flip(s, :, :) = mean(squeeze(EEG.data(:, :, idx_hard_flip)), 3);
+
+end
+
+% Select frontal channels
+frontal_channel_idx = [65, 15, 16, 19, 20];
+
+% Calculate frontal ERPs. Data is then subject x times dimensionality.
+erp_frontal_easy_accu = squeeze(mean(erp_easy_accu(:, frontal_channel_idx, :), 2));
+erp_frontal_easy_flip = squeeze(mean(erp_easy_flip(:, frontal_channel_idx, :), 2));
+erp_frontal_hard_accu = squeeze(mean(erp_hard_accu(:, frontal_channel_idx, :), 2));
+erp_frontal_hard_flip = squeeze(mean(erp_hard_flip(:, frontal_channel_idx, :), 2));
+
+% Define time window for CNV-parameterization
+cnv_times = [700, 900];
+
+% Get cnv-time indices
+cnv_time_idx = EEG.times >= cnv_times(1) & EEG.times <= cnv_times(2);
+
+%% ========================= PLOTTING ======================================================================================================================
+
+% Plot frontal ERPs
+figure()
+plot(EEG.times, mean(erp_frontal_easy_accu, 1), 'k-', 'LineWidth', 2)
+hold on;
+plot(EEG.times, mean(erp_frontal_easy_flip, 1), 'k:', 'LineWidth', 2)
+plot(EEG.times, mean(erp_frontal_hard_accu, 1), 'r-', 'LineWidth', 2)
+plot(EEG.times, mean(erp_frontal_hard_flip, 1), 'r:', 'LineWidth', 2)
+legend({'easy accu', 'easy flip', 'hard accu', 'hard flip'})
+title('frontal channels')
+xline([0, 1200])
+ylims = [-5, 3];
+ylim(ylims)
+xlim([-200, 2000])
+rectangle('Position', [cnv_times(1), ylims(1), cnv_times(2) - cnv_times(1), ylims(2) - ylims(1)], 'FaceColor',[0.5, 1, 0.5, 0.2], 'EdgeColor', 'none')
+
+% Average over subjects and cnv-times to plot topographies
+topo_easy_accu = squeeze(mean(erp_easy_accu(:, :, cnv_time_idx), [1, 3]));
+topo_easy_flip = squeeze(mean(erp_easy_flip(:, :, cnv_time_idx), [1, 3]));
+topo_hard_accu = squeeze(mean(erp_hard_accu(:, :, cnv_time_idx), [1, 3]));
+topo_hard_flip = squeeze(mean(erp_hard_flip(:, :, cnv_time_idx), [1, 3]));
+
+% Create difference topographies for main-effects
+topo_difficulty = ((topo_hard_accu + topo_hard_flip) / 2) - ((topo_easy_accu + topo_easy_flip) / 2);
+topo_reliability = ((topo_easy_accu + topo_hard_accu) / 2) - ((topo_easy_flip + topo_hard_flip) / 2) ;
+
+% Plot topos
+figure()
+
+subplot(3, 2, 1)
+topoplot(topo_easy_accu, EEG.chanlocs, 'plotrad', 0.7, 'intrad', 0.7, 'intsquare', 'on', 'conv', 'off', 'electrodes', 'on');
+colormap('jet')
+set(gca, 'clim', [-3, 3])
+colorbar;
+title(['easy accu'], 'FontSize', 10)
+
+subplot(3, 2, 2)
+topoplot(topo_easy_flip, EEG.chanlocs, 'plotrad', 0.7, 'intrad', 0.7, 'intsquare', 'on', 'conv', 'off', 'electrodes', 'on');
+colormap('jet')
+set(gca, 'clim', [-3, 3])
+colorbar;
+title(['easy flip'], 'FontSize', 10)
+
+subplot(3, 2, 3)
+topoplot(topo_hard_accu, EEG.chanlocs, 'plotrad', 0.7, 'intrad', 0.7, 'intsquare', 'on', 'conv', 'off', 'electrodes', 'on');
+colormap('jet')
+set(gca, 'clim', [-3, 3])
+colorbar;
+title(['hard accu'], 'FontSize', 10)
+
+subplot(3, 2, 4)
+topoplot(topo_hard_flip, EEG.chanlocs, 'plotrad', 0.7, 'intrad', 0.7, 'intsquare', 'on', 'conv', 'off', 'electrodes', 'on');
+colormap('jet')
+set(gca, 'clim', [-3, 3])
+colorbar;
+title(['hard flip'], 'FontSize', 10)
+
+subplot(3, 2, 5)
+topoplot(topo_difficulty, EEG.chanlocs, 'plotrad', 0.7, 'intrad', 0.7, 'intsquare', 'on', 'conv', 'off', 'electrodes', 'on');
+colormap('jet')
+set(gca, 'clim', [-1, 1])
+colorbar;
+title(['hard - easy'], 'FontSize', 10)
+
+subplot(3, 2, 6)
+topoplot(topo_reliability, EEG.chanlocs, 'plotrad', 0.7, 'intrad', 0.7, 'intsquare', 'on', 'conv', 'off', 'electrodes', 'on');
+colormap('jet')
+set(gca, 'clim', [-1, 1])
+colorbar;
+title(['accu - flip'], 'FontSize', 10)
+
+%% ========================= STATISTICS ======================================================================================================================
+
+% Average CNV valus in time window for each subject
+cnv_values = [mean(erp_frontal_easy_accu(:, cnv_time_idx), 2),...
+              mean(erp_frontal_easy_flip(:, cnv_time_idx), 2),...
+              mean(erp_frontal_hard_accu(:, cnv_time_idx), 2),...
+              mean(erp_frontal_hard_flip(:, cnv_time_idx), 2)];
+
+% Perform rmANOVA for CNV-values
+varnames = {'subject', 'easy_accu', 'easy_flip', 'hard_accu', 'hard_flip'};
+t = table(ids', cnv_values(:, 1), cnv_values(:, 2), cnv_values(:, 3), cnv_values(:, 4), 'VariableNames', varnames);
+within = table({'easy'; 'easy'; 'hard'; 'hard'}, {'accu'; 'flip'; 'accu'; 'flip'}, 'VariableNames', {'difficulty', 'reliability'});
+rm = fitrm(t, 'easy_accu-hard_flip~1', 'WithinDesign', within);
+anova_cnv = ranova(rm, 'WithinModel', 'difficulty + reliability + difficulty*reliability');
+anova_cnv
+
---
+++ eeg_seminar_prepro.m
@@ -0,0 +1,260 @@
+clear all;
+
+% PATH VARS
+PATH_EEGLAB = '/home/plkn/eeglab2022.1/';
+PATH_RAW = '/mnt/data_dump/pixelflip/0_raw/';
+PATH_ICSET = '/mnt/data_dump/pixelflip/1_icset/';
+PATH_AUTOCLEANED = '/mnt/data_dump/pixelflip/2_cleaned/';
+
+% Initialize eeglab
+addpath(PATH_EEGLAB);
+eeglab;
+
+%% =============== LOAD DATA ======================================================================================================================
+
+% Select subject to process
+% Subjects available: 'VP01', 'VP02', 'VP03', 'VP05', 'VP06', 'VP08', 'VP12', 'VP07', 'VP11', 'VP09', 'VP16', 'VP17'
+subject = 'VP17';
+
+% Get subject identifier as a number
+id = str2num(subject(3 : 4));
+
+% Load the raw data as recorded
+EEG = pop_loadbv(PATH_RAW, [subject, '.vhdr'], [], []);
+
+% Redraw eeglab to inspect data
+eeglab redraw;
+
+%% =============== EVENT CODING ===================================================================================================================
+
+% Iterate events in "EEG.event"
+trialinfo = [];
+block_nr = 0;
+trial_nr = 0;
+for e = 1 : length(EEG.event)
+
+    % If an S event
+    if strcmpi(EEG.event(e).type(1), 'S')
+
+        % Get event number
+        enum = str2num(EEG.event(e).type(2 : end));
+
+        % Set block number
+        if enum >= 101 & enum <= 207 & trial_nr >= 20
+            block_nr = str2num(EEG.event(e).type(end));
+        end
+
+        % If cue-stimulus
+        if enum >= 11 & enum <= 18
+
+            % Increase trial counter
+            trial_nr = trial_nr + 1;
+
+            % Get reliability
+            if ismember(enum, [11, 12, 13, 14])
+                reliability = 1; % Relaible
+            else
+                reliability = 0; % unreliable
+            end
+
+            % Get difficulty
+            if ismember(enum, [11, 12, 15, 16])
+                difficulty = 0; % easy
+            else
+                difficulty = 1; % hard
+            end
+
+            % Get flipped
+            if ismember(enum, [12, 14, 16, 18])
+                flipped = 1; % flipped
+            else
+                flipped = 0; % not flipped
+            end
+
+            % Loop to feedback
+            f = e;
+            rt = NaN;
+            key_pressed = 0;
+            while ~ismember(str2num(EEG.event(f).type(2 : end)), [30, 31, 40, 41, 50])
+
+                % If response left
+                if str2num(EEG.event(f).type(2 : end)) == 5 
+                    key_pressed = 1;
+                    rt = EEG.event(f).latency - EEG.event(e).latency - 1200;
+                end
+
+                % If response right
+                if str2num(EEG.event(f).type(2 : end)) == 4
+                    key_pressed = 2;
+                    rt = EEG.event(f).latency - EEG.event(e).latency - 1200;
+                end
+
+                % Move on
+                f = f + 1;
+
+            end
+
+            % Check color pressed
+            color_pressed = 0;
+            if key_pressed == 1 & mod(id, 2) == 1 % Pressed left odd id
+                color_pressed = 1; % cyan
+            elseif key_pressed == 1 & mod(id, 2) == 0 % Pressed left even id
+                color_pressed = 2; % orange
+            elseif key_pressed == 2 & mod(id, 2) == 1 % Pressed right odd id
+                color_pressed = 2; % orange
+            elseif key_pressed == 2 & mod(id, 2) == 0 % Pressed right even id
+                color_pressed = 1; % cyan
+            end
+
+            % Get feedback color and feedback accuracy
+            if str2num(EEG.event(f).type(2 : end)) == 30
+                feedback_accuracy = 1; % correct
+                feedback_color = 2; % orange
+            elseif str2num(EEG.event(f).type(2 : end)) == 31
+                feedback_accuracy = 1; % correct
+                feedback_color = 1; % cyan
+            elseif str2num(EEG.event(f).type(2 : end)) == 40
+                feedback_accuracy = 0; % false
+                feedback_color = 2; % orange
+            elseif str2num(EEG.event(f).type(2 : end)) == 41
+                feedback_accuracy = 0; % false
+                feedback_color = 1; % cyan
+            elseif str2num(EEG.event(f).type(2 : end)) == 50
+                feedback_accuracy = -1; % missing
+                feedback_color = 0; % none
+            end
+
+            % Get actual accuracy
+            if feedback_accuracy == 1 & flipped == 0
+                accuracy = 1;
+            elseif feedback_accuracy == 1 & flipped == 1
+                accuracy = 0;
+            elseif feedback_accuracy == 0 & flipped == 0
+                accuracy = 0;
+            elseif feedback_accuracy == 0 & flipped == 1
+                accuracy = 1;
+            elseif feedback_accuracy == -1
+                accuracy = -1; % missing
+            end
+
+            % Save info
+            trialinfo(trial_nr, :) = [trial_nr, block_nr, reliability, difficulty, flipped, key_pressed, rt, color_pressed, feedback_accuracy, feedback_color, accuracy];
+
+            % Rename events
+            EEG.event(e).type = 'cue';
+            EEG.event(e).code = 'cue';
+
+        end
+    end
+end
+
+% Trialinfo columns:
+% 01: trial_nr
+% 02: block_nr
+% 03: reliability
+% 04: difficulty
+% 05: flipped
+% 06: key_pressed
+% 07: rt
+% 08: color_pressed
+% 09: feedback_accuracy
+% 10: feedback_color
+% 11: accuracy
+
+% Add trialinfo to EEG struct
+EEG.trialinfo = trialinfo;
+
+% Redraw eeglab to inspect data
+eeglab redraw;
+
+%% =============== PREPROCESSING OF CONTINUOUS DATA ===================================================================================================================
+
+% Add FCz, the online reference channel, as an empty channel
+EEG.data(end + 1, :) = 0;
+EEG.nbchan = size(EEG.data, 1);
+EEG.chanlocs(end + 1).labels = 'FCz';
+
+% Select the file containing info about electrode positions
+channel_location_file = which('standard-10-5-cap385.elp');
+
+% Add channel locations from file
+EEG = pop_chanedit(EEG, 'lookup', channel_location_file);
+
+% Save original channel locations (for later interpolation)
+EEG.chanlocs_original = EEG.chanlocs;
+
+% Rereference data to CPz, so that FCz contains non-interpolated data
+EEG = pop_reref(EEG, 'CPz');
+
+% Resample data
+EEG = pop_resample(EEG, 200);
+
+% Filter data for ICA
+ICA = pop_basicfilter(ICA, [1 : ICA.nbchan], 'Cutoff', [2, 30], 'Design', 'butter', 'Filter', 'bandpass', 'Order', 6, 'RemoveDC', 'on', 'Boundary', 'boundary');
+
+% Filter data for ERP analysis
+EEG = pop_basicfilter(EEG, [1 : EEG.nbchan], 'Cutoff', [0.01, 30], 'Design', 'butter', 'Filter', 'bandpass', 'Order', 6, 'RemoveDC', 'on', 'Boundary', 'boundary'); 
+
+% Detect and reject bad channels
+[EEG, EEG.chans_rejected] = pop_rejchan(EEG, 'elec', [1 : EEG.nbchan], 'threshold', 5, 'norm', 'on', 'measure', 'kurt');
+[ICA, ICA.chans_rejected] = pop_rejchan(ICA, 'elec', [1 : ICA.nbchan], 'threshold', 5, 'norm', 'on', 'measure', 'kurt');
+
+% Interpolate rejected channels
+EEG = pop_interp(EEG, EEG.chanlocs_original, 'spherical');
+ICA = pop_interp(ICA, ICA.chanlocs_original, 'spherical');
+
+% Rereference data to common average reference
+EEG = pop_reref(EEG, []);
+ICA = pop_reref(ICA, []);
+
+% Determine rank of data
+dataRank = sum(eig(cov(double(ICA.data'))) > 1e-6);
+
+% Redraw eeglab to inspect data
+eeglab redraw;
+
+%% =============== EPOCH DATA & PREPROCESSING OF EPOCHED DATA ===================================================================================================
+
+% Epoch EEG data cue-locked
+EEG = pop_epoch(EEG, {'cue'}, [-0.3, 2], 'newname', [subject '_epoched'], 'epochinfo', 'yes');
+ICA = pop_epoch(ICA, {'cue'}, [-0.3, 2], 'newname', [subject '_epoched'], 'epochinfo', 'yes');
+
+% Remove baseline
+EEG = pop_rmbase(EEG, [-200, 0]);
+ICA = pop_rmbase(ICA, [-200, 0]);
+
+% Automatically reject trials
+[EEG, EEG.rejected_epochs] = pop_autorej(EEG, 'nogui', 'on');
+[ICA, ICA.rejected_epochs] = pop_autorej(ICA, 'nogui', 'on');
+
+% Remove rejected trials from trialinfo
+EEG.trialinfo(EEG.rejected_epochs, :) = [];
+ICA.trialinfo(ICA.rejected_epochs, :) = [];
+
+% Redraw eeglab to inspect data
+eeglab redraw;
+
+%% =============== RUN INDEPENDENT COMPONENT ANALYSIS ===================================================================================================
+
+% Run independent component analysis (ICA) on ICA-data
+ICA = pop_runica(ICA, 'extended', 1, 'interrupt', 'on', 'PCA', dataRank - 5);
+
+% Classify the independent components (ICs)
+ICA = iclabel(ICA);
+
+% Find eye-activity related ICs
+ICA.nobrainer = find(ICA.etc.ic_classification.ICLabel.classifications(:, 3) > 0.3);
+
+% Copy ICs to ERP-dataset
+EEG = pop_editset(EEG, 'icachansind', 'ICA.icachansind', 'icaweights', 'ICA.icaweights', 'icasphere', 'ICA.icasphere');
+EEG.etc = ICA.etc;
+EEG.nobrainer = ICA.nobrainer;
+
+% Save IC set
+pop_saveset(EEG, 'filename', [subject, '_icset_cue_erp.set'], 'filepath', PATH_ICSET, 'check', 'on');
+
+% Remove eye-related ICs
+EEG = pop_subcomp(EEG, EEG.nobrainer, 0);
+
+% Save cleaned data
+pop_saveset(EEG, 'filename', [subject, '_cleaned_cue_erp.set'], 'filepath', PATH_AUTOCLEANED, 'check', 'on');
