37c4e155f6a1fd1a0879bba43447241954cbde72
plkn <fischmechanik@gmail.com>
2023-12-18 16:33:14 +0100

    feedback erp finished

--- pixelflip_feedbacklocked_erp_clustertest_state_and_sequence.m
+++ pixelflip_feedbacklocked_erp_clustertest_state_and_sequence.m
@@ -1,9 +1,9 @@
 clear all;
 
-% PATH VARS - PLEASE ADJUST!!!!!
+% Path variables
 PATH_EEGLAB      = '/home/plkn/eeglab2022.1/';
 PATH_AUTOCLEANED = '/mnt/data_dump/pixelflip/2_cleaned/';
-PATH_VEUSZ       = '/mnt/data_dump/pixelflip/veusz/feedback_erp_sequence/';  
+PATH_VEUSZ       = '/mnt/data_dump/pixelflip/veusz/feedback_erp_state/';  
 
 % Subject list
 subject_list = {'VP01', 'VP02', 'VP03', 'VP04', 'VP05', 'VP06', 'VP07', 'VP08', 'VP09', 'VP10',...
@@ -52,8 +52,10 @@ for s = 1 : length(subject_list)
     ids(s) = str2num(subject(3 : 4));
 
     % Load subject data. EEG data has dimensionality channels x times x trials
-    EEG = pop_loadset('filename',    [subject, '_cleaned_feedback_erp.set'], 'filepath', PATH_AUTOCLEANED, 'loadmode', 'all');
+    EEG = pop_loadset('filename', [subject, '_cleaned_feedback_erp.set'], 'filepath', PATH_AUTOCLEANED, 'loadmode', 'all');
 
+    % Load cuelocked data for baseline values
+    BLVALS = pop_loadset('filename', [subject, '_cleaned_cue_erp.set'], 'filepath', PATH_AUTOCLEANED, 'check', 'on');
 
     % Trialinfo columns:
     % 01: trial_nr
@@ -132,6 +134,17 @@ for s = 1 : length(subject_list)
     
         end
     end
+
+    % Find trials that are present in both, feedbacklocked and cuelocked, datasets
+    common_trials = intersect(BLVALS.trialinfo(:, 1), EEG.trialinfo(:, 1));
+
+    % Reduce datasets to common trials
+    to_keep = ismember(EEG.trialinfo(:, 1), common_trials); 
+    EEG.data = EEG.data(:, :, to_keep);
+    EEG.trialinfo = EEG.trialinfo(to_keep, :);
+    to_keep = ismember(BLVALS.trialinfo(:, 1), common_trials); 
+    BLVALS.data = BLVALS.data(:, :, to_keep);
+    BLVALS.trialinfo = BLVALS.trialinfo(to_keep, :);
     
     % Get trial-indices of conditions
     idx_flip0_easy_flip0 = EEG.trialinfo(:, 11) == 1 & EEG.trialinfo(:, 3) == 1 & EEG.trialinfo(:, 4) == 0;
@@ -141,13 +154,34 @@ for s = 1 : length(subject_list)
     idx_flip1_easy_flip1 = EEG.trialinfo(:, 11) == 1 & EEG.trialinfo(:, 3) == 0 & EEG.trialinfo(:, 4) == 0 & EEG.trialinfo(:, 5) == 1;
     idx_flip1_hard_flip1 = EEG.trialinfo(:, 11) == 1 & EEG.trialinfo(:, 3) == 0 & EEG.trialinfo(:, 4) == 1 & EEG.trialinfo(:, 5) == 1;
 
+    % Get condition specific baseline values for feedbacklocked erps from before cue-onset
+    idx_bl = BLVALS.times >= -200 & BLVALS.times <= 0;
+    bl_flip0_easy_flip0(s, :, :) = squeeze(mean(BLVALS.data(:, idx_bl, idx_flip0_easy_flip0), [2, 3]));
+    bl_flip0_hard_flip0(s, :, :) = squeeze(mean(BLVALS.data(:, idx_bl, idx_flip0_hard_flip0), [2, 3]));
+    bl_flip1_easy_flip0(s, :, :) = squeeze(mean(BLVALS.data(:, idx_bl, idx_flip1_easy_flip0), [2, 3]));
+    bl_flip1_hard_flip0(s, :, :) = squeeze(mean(BLVALS.data(:, idx_bl, idx_flip1_hard_flip0), [2, 3]));
+    bl_flip1_easy_flip1(s, :, :) = squeeze(mean(BLVALS.data(:, idx_bl, idx_flip1_easy_flip1), [2, 3]));
+    bl_flip1_hard_flip1(s, :, :) = squeeze(mean(BLVALS.data(:, idx_bl, idx_flip1_hard_flip1), [2, 3]));   
+
     % Calculate subject ERPs by averaging across trials for each condition.
     erp_flip0_easy_flip0(s, :, :) = mean(squeeze(EEG.data(:, erp_times_idx, idx_flip0_easy_flip0)), 3);
     erp_flip0_hard_flip0(s, :, :) = mean(squeeze(EEG.data(:, erp_times_idx, idx_flip0_hard_flip0)), 3);
     erp_flip1_easy_flip0(s, :, :) = mean(squeeze(EEG.data(:, erp_times_idx, idx_flip1_easy_flip0)), 3);
     erp_flip1_hard_flip0(s, :, :) = mean(squeeze(EEG.data(:, erp_times_idx, idx_flip1_hard_flip0)), 3);
     erp_flip1_easy_flip1(s, :, :) = mean(squeeze(EEG.data(:, erp_times_idx, idx_flip1_easy_flip1)), 3);
-    erp_flip1_hard_flip1(s, :, :) = mean(squeeze(EEG.data(:, erp_times_idx, idx_flip1_hard_flip1)), 3);   
+    erp_flip1_hard_flip1(s, :, :) = mean(squeeze(EEG.data(:, erp_times_idx, idx_flip1_hard_flip1)), 3);
+
+    % Subtract baseline
+    for ch = 1 : EEG.nbchan
+        
+        erp_flip0_easy_flip0(s, ch, :) = squeeze(erp_flip0_easy_flip0(s, ch, :)) - squeeze(bl_flip0_easy_flip0(s, ch, :));
+        erp_flip0_hard_flip0(s, ch, :) = squeeze(erp_flip0_hard_flip0(s, ch, :)) - squeeze(bl_flip0_hard_flip0(s, ch, :));
+        erp_flip1_easy_flip0(s, ch, :) = squeeze(erp_flip1_easy_flip0(s, ch, :)) - squeeze(bl_flip1_easy_flip0(s, ch, :));
+        erp_flip1_hard_flip0(s, ch, :) = squeeze(erp_flip1_hard_flip0(s, ch, :)) - squeeze(bl_flip1_hard_flip0(s, ch, :));
+        erp_flip1_easy_flip1(s, ch, :) = squeeze(erp_flip1_easy_flip1(s, ch, :)) - squeeze(bl_flip1_easy_flip1(s, ch, :));
+        erp_flip1_hard_flip1(s, ch, :) = squeeze(erp_flip1_hard_flip1(s, ch, :)) - squeeze(bl_flip1_hard_flip1(s, ch, :)); 
+
+    end
 
 end
 
@@ -270,61 +304,59 @@ ga_template.dimord = 'chan_time';
 ga_template.label = chanlabs;
 ga_template.time = erp_times;
 
-% ############################### noflip vs. flip ###########################################
-
-% GA struct easy
+% GA struct easy noflip
 GA = {};
 for s = 1 : length(subject_list)
-    chan_time_data = (squeeze(erp_flip1_easy_flip1(s, :, :)) + squeeze(erp_flip1_easy_flip0(s, :, :))) ./ 2;
+    chan_time_data = squeeze(erp_flip0_easy_flip0(s, :, :));
     ga_template.avg = chan_time_data;
     GA{s} = ga_template;
 end 
-GA_easy = ft_timelockgrandaverage(cfg, GA{1, :});
+GA_easy_noflip = ft_timelockgrandaverage(cfg, GA{1, :});
 
-% GA struct hard
+% GA struct easy flip
 GA = {};
 for s = 1 : length(subject_list)
-    chan_time_data = (squeeze(erp_flip1_hard_flip1(s, :, :)) + squeeze(erp_flip1_hard_flip0(s, :, :))) ./ 2;
+    chan_time_data = squeeze(erp_flip1_easy_flip0(s, :, :));
     ga_template.avg = chan_time_data;
     GA{s} = ga_template;
 end 
-GA_hard = ft_timelockgrandaverage(cfg, GA{1, :});
+GA_easy_flip = ft_timelockgrandaverage(cfg, GA{1, :});
 
-% GA struct noflip
+% GA struct easy flipflip
 GA = {};
 for s = 1 : length(subject_list)
-    chan_time_data = (squeeze(erp_flip1_easy_flip0(s, :, :)) + squeeze(erp_flip1_hard_flip0(s, :, :))) ./ 2;
+    chan_time_data = squeeze(erp_flip1_easy_flip1(s, :, :));
     ga_template.avg = chan_time_data;
     GA{s} = ga_template;
 end 
-GA_noflip = ft_timelockgrandaverage(cfg, GA{1, :});
+GA_easy_flipflip = ft_timelockgrandaverage(cfg, GA{1, :});
 
-% GA struct flip
+% GA struct hard noflip
 GA = {};
 for s = 1 : length(subject_list)
-    chan_time_data = (squeeze(erp_flip1_easy_flip1(s, :, :)) + squeeze(erp_flip1_hard_flip1(s, :, :))) ./ 2;
+    chan_time_data = squeeze(erp_flip0_hard_flip0(s, :, :));
     ga_template.avg = chan_time_data;
     GA{s} = ga_template;
 end 
-GA_flip = ft_timelockgrandaverage(cfg, GA{1, :});
+GA_hard_noflip = ft_timelockgrandaverage(cfg, GA{1, :});
 
-% GA struct hard minus easy flip
+% GA struct hard flip
 GA = {};
 for s = 1 : length(subject_list)
-    chan_time_data = squeeze(erp_flip1_hard_flip0(s, :, :)) - squeeze(erp_flip1_easy_flip0(s, :, :));
+    chan_time_data = squeeze(erp_flip1_hard_flip0(s, :, :));
     ga_template.avg = chan_time_data;
     GA{s} = ga_template;
 end 
-GA_interaction_noflip = ft_timelockgrandaverage(cfg, GA{1, :});
+GA_hard_flip = ft_timelockgrandaverage(cfg, GA{1, :});
 
-% GA struct hard minus easy noflip
+% GA struct hard flipflip
 GA = {};
 for s = 1 : length(subject_list)
-    chan_time_data = squeeze(erp_flip1_hard_flip1(s, :, :)) - squeeze(erp_flip1_easy_flip1(s, :, :));
+    chan_time_data = squeeze(erp_flip1_hard_flip1(s, :, :));
     ga_template.avg = chan_time_data;
     GA{s} = ga_template;
 end 
-GA_interaction_flip = ft_timelockgrandaverage(cfg, GA{1, :});
+GA_hard_flipflip = ft_timelockgrandaverage(cfg, GA{1, :});
 
 % Testparams
 testalpha  = 0.05;
@@ -357,34 +389,72 @@ cfg.design = design;
 
 
 % The tests
-[stat_difficulty]  = ft_timelockstatistics(cfg, GA_easy, GA_hard);
-[stat_agency]      = ft_timelockstatistics(cfg, GA_noflip, GA_flip);
-[stat_interaction] = ft_timelockstatistics(cfg, GA_interaction_noflip, GA_interaction_flip);
+[stat_agency_easy] = ft_timelockstatistics(cfg, GA_easy_noflip, GA_easy_flip);
+[stat_agency_hard] = ft_timelockstatistics(cfg, GA_hard_noflip, GA_hard_flip);
+[stat_agency_sequence_easy] = ft_timelockstatistics(cfg, GA_easy_flip, GA_easy_flipflip);
+[stat_agency_sequence_hard] = ft_timelockstatistics(cfg, GA_hard_flip, GA_hard_flipflip);
 
 % Save masks
-dlmwrite([PATH_VEUSZ, 'contour_difficulty.csv'], stat_difficulty.mask);
-dlmwrite([PATH_VEUSZ, 'contour_agency.csv'], stat_agency.mask);
-dlmwrite([PATH_VEUSZ, 'contour_interaction.csv'], stat_interaction.mask);
+dlmwrite([PATH_VEUSZ, 'contour_agency_easy.csv'], stat_agency_easy.mask);
+dlmwrite([PATH_VEUSZ, 'contour_agency_hard.csv'], stat_agency_hard.mask);
 
 % Calculate effect sizes
 n_chans = numel(chanlocs);
-apes_difficulty  = [];
-apes_agency      = [];
-apes_interaction = [];
+apes_agency_easy = [];
+apes_agency_hard = [];
+apes_agency_sequence_easy = [];
+apes_agency_sequence_hard = [];
 df_effect = 1;
 for ch = 1 : n_chans
-    petasq = (squeeze(stat_difficulty.stat(ch, :)) * df_effect) ./ ((squeeze(stat_difficulty.stat(ch, :)) * df_effect) + (n_subjects - 1));
-    apes_difficulty(ch, :) = petasq - (1 - petasq) .* (df_effect / (n_subjects - 1));
-    petasq = (squeeze(stat_agency.stat(ch, :)) * df_effect) ./ ((squeeze(stat_agency.stat(ch, :)) * df_effect) + (n_subjects - 1));
-    apes_agency(ch, :) = petasq - (1 - petasq) .* (df_effect / (n_subjects - 1));
-    petasq = (squeeze(stat_interaction.stat(ch, :)) * df_effect) ./ ((squeeze(stat_interaction.stat(ch, :)) * df_effect) + (n_subjects - 1));
-    apes_interaction(ch, :) = petasq - (1 - petasq) .* (df_effect / (n_subjects - 1));
+    petasq = (squeeze(stat_agency_easy.stat(ch, :)) * df_effect) ./ ((squeeze(stat_agency_easy.stat(ch, :)) * df_effect) + (n_subjects - 1));
+    apes_agency_easy(ch, :) = petasq - (1 - petasq) .* (df_effect / (n_subjects - 1));
+    petasq = (squeeze(stat_agency_hard.stat(ch, :)) * df_effect) ./ ((squeeze(stat_agency_hard.stat(ch, :)) * df_effect) + (n_subjects - 1));
+    apes_agency_hard(ch, :) = petasq - (1 - petasq) .* (df_effect / (n_subjects - 1));
+    petasq = (squeeze(stat_agency_sequence_easy.stat(ch, :)) * df_effect) ./ ((squeeze(stat_agency_sequence_easy.stat(ch, :)) * df_effect) + (n_subjects - 1));
+    apes_agency_sequence_easy(ch, :) = petasq - (1 - petasq) .* (df_effect / (n_subjects - 1));
+    petasq = (squeeze(stat_agency_sequence_hard.stat(ch, :)) * df_effect) ./ ((squeeze(stat_agency_sequence_hard.stat(ch, :)) * df_effect) + (n_subjects - 1));
+    apes_agency_sequence_hard(ch, :) = petasq - (1 - petasq) .* (df_effect / (n_subjects - 1));
 end
 
+% Plot masks
+figure()
+subplot(2, 1, 1)
+contourf(erp_times,[1:65], apes_agency_easy, 50, 'LineColor', 'none')
+caxis([-0.5, 0.5])
+colormap(jet)
+hold on
+contour(erp_times,[1:65], stat_agency_easy.mask, 'levels', 1, 'LineColor', 'k', 'LineWidth', 1.5)
+title('agency state easy')
+
+subplot(2, 1, 2)
+contourf(erp_times,[1:65], apes_agency_hard, 50, 'LineColor', 'none')
+caxis([-0.5, 0.5])
+colormap(jet)
+hold on
+contour(erp_times,[1:65], stat_agency_hard.mask, 'levels', 1, 'LineColor', 'k', 'LineWidth', 1.5)
+title('agency state hard')
+
+% Plot masks
+figure()
+subplot(2, 1, 1)
+contourf(erp_times,[1:65], apes_agency_sequence_easy, 50, 'LineColor', 'none')
+caxis([-0.5, 0.5])
+colormap(jet)
+hold on
+contour(erp_times,[1:65], stat_agency_sequence_easy.mask, 'levels', 1, 'LineColor', 'k', 'LineWidth', 1.5)
+title('agency sequence easy')
+
+subplot(2, 1, 2)
+contourf(erp_times,[1:65], apes_agency_sequence_hard, 50, 'LineColor', 'none')
+caxis([-0.5, 0.5])
+colormap(jet)
+hold on
+contour(erp_times,[1:65], stat_agency_sequence_hard.mask, 'levels', 1, 'LineColor', 'k', 'LineWidth', 1.5)
+title('agency sequence hard')
+
 % Save effect sizes
-dlmwrite([PATH_VEUSZ, 'apes_difficulty.csv'], apes_difficulty);
-dlmwrite([PATH_VEUSZ, 'apes_agency.csv'], apes_agency);
-dlmwrite([PATH_VEUSZ, 'apes_interaction.csv'], apes_interaction);
+dlmwrite([PATH_VEUSZ, 'apes_agency_easy.csv'], apes_agency_easy);
+dlmwrite([PATH_VEUSZ, 'apes_agency_hard.csv'], apes_agency_hard);
 
 % Save lineplots at Fz
 dlmwrite([PATH_VEUSZ, 'lineplots_fz.csv'],  [mean(squeeze(erp_flip0_easy_flip0(:, 11, :)), 1);...
@@ -404,11 +474,11 @@ dlmwrite([PATH_VEUSZ, 'lineplots_fcz.csv'],  [mean(squeeze(erp_flip0_easy_flip0(
 
 % Save lineplots at Cz
 dlmwrite([PATH_VEUSZ, 'lineplots_cz.csv'],  [mean(squeeze(erp_flip0_easy_flip0(:, 29, :)), 1);...
-                                              mean(squeeze(erp_flip0_hard_flip0(:, 29, :)), 1);...
-                                              mean(squeeze(erp_flip1_easy_flip0(:, 29, :)), 1);...
-                                              mean(squeeze(erp_flip1_hard_flip0(:, 29, :)), 1);...
-                                              mean(squeeze(erp_flip1_easy_flip1(:, 29, :)), 1);...
-                                              mean(squeeze(erp_flip1_hard_flip1(:, 29, :)), 1)]);
+                                             mean(squeeze(erp_flip0_hard_flip0(:, 29, :)), 1);...
+                                             mean(squeeze(erp_flip1_easy_flip0(:, 29, :)), 1);...
+                                             mean(squeeze(erp_flip1_hard_flip0(:, 29, :)), 1);...
+                                             mean(squeeze(erp_flip1_easy_flip1(:, 29, :)), 1);...
+                                             mean(squeeze(erp_flip1_hard_flip1(:, 29, :)), 1)]);
 
 % Save lineplots at CPz
 dlmwrite([PATH_VEUSZ, 'lineplots_cpz.csv'],  [mean(squeeze(erp_flip0_easy_flip0(:, 38, :)), 1);...
@@ -433,12 +503,111 @@ dlmwrite([PATH_VEUSZ, 'lineplots_poz.csv'],  [mean(squeeze(erp_flip0_easy_flip0(
                                               mean(squeeze(erp_flip1_easy_flip1(:, 57, :)), 1);...
                                               mean(squeeze(erp_flip1_hard_flip1(:, 57, :)), 1)]);
 
+
 % Save erp-times
 dlmwrite([PATH_VEUSZ, 'erp_times.csv'], erp_times);
 
+% Plot erps
+pd_fz = [mean(squeeze(erp_flip0_easy_flip0(:, 11, :)), 1);...
+         mean(squeeze(erp_flip0_hard_flip0(:, 11, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip0(:, 11, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip0(:, 11, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip1(:, 11, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip1(:, 11, :)), 1)];
+
+pd_fcz = [mean(squeeze(erp_flip0_easy_flip0(:, 20, :)), 1);...
+          mean(squeeze(erp_flip0_hard_flip0(:, 20, :)), 1);...
+          mean(squeeze(erp_flip1_easy_flip0(:, 20, :)), 1);...
+          mean(squeeze(erp_flip1_hard_flip0(:, 20, :)), 1);...
+          mean(squeeze(erp_flip1_easy_flip1(:, 20, :)), 1);...
+          mean(squeeze(erp_flip1_hard_flip1(:, 20, :)), 1)];
+
+pd_cz = [mean(squeeze(erp_flip0_easy_flip0(:, 29, :)), 1);...
+         mean(squeeze(erp_flip0_hard_flip0(:, 29, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip0(:, 29, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip0(:, 29, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip1(:, 29, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip1(:, 29, :)), 1)];
+
+pd_cpz = [mean(squeeze(erp_flip0_easy_flip0(:, 38, :)), 1);...
+         mean(squeeze(erp_flip0_hard_flip0(:, 38, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip0(:, 38, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip0(:, 38, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip1(:, 38, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip1(:, 38, :)), 1)];
+
+pd_pz = [mean(squeeze(erp_flip0_easy_flip0(:, 48, :)), 1);...
+         mean(squeeze(erp_flip0_hard_flip0(:, 48, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip0(:, 48, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip0(:, 48, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip1(:, 48, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip1(:, 48, :)), 1)];
+
+pd_poz = [mean(squeeze(erp_flip0_easy_flip0(:, 57, :)), 1);...
+         mean(squeeze(erp_flip0_hard_flip0(:, 57, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip0(:, 57, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip0(:, 57, :)), 1);...
+         mean(squeeze(erp_flip1_easy_flip1(:, 57, :)), 1);...
+         mean(squeeze(erp_flip1_hard_flip1(:, 57, :)), 1)];
+
+figure()
+subplot(3, 2, 1)
+plot(erp_times, pd_fz([1,3,5], :), 'LineWidth', 1.5)
+title('easy Fz')
+xlim([-200, 1000])
+subplot(3, 2, 2)
+plot(erp_times, pd_fcz([1,3,5], :), 'LineWidth', 1.5)
+title('FCz')
+xlim([-200, 1000])
+subplot(3, 2, 3)
+plot(erp_times, pd_cz([1,3,5], :), 'LineWidth', 1.5)
+title('Cz')
+xlim([-200, 1000])
+subplot(3, 2, 4)
+plot(erp_times, pd_cpz([1,3,5], :), 'LineWidth', 1.5)
+title('CPz')
+xlim([-200, 1000])
+subplot(3, 2, 5)
+plot(erp_times, pd_pz([1,3,5], :), 'LineWidth', 1.5)
+title('Pz')
+xlim([-200, 1000])
+subplot(3, 2, 6)
+plot(erp_times, pd_poz([1,3,5], :), 'LineWidth', 1.5)
+title('POz')
+xlim([-200, 1000])
+legend({'00', '10', '11'})
+
+figure()
+subplot(3, 2, 1)
+plot(erp_times, pd_fz([2, 4, 6], :), 'LineWidth', 1.5)
+title('hard Fz')
+xlim([-200, 1000])
+subplot(3, 2, 2)
+plot(erp_times, pd_fcz([2, 4, 6], :), 'LineWidth', 1.5)
+title('FCz')
+xlim([-200, 1000])
+subplot(3, 2, 3)
+plot(erp_times, pd_cz([2, 4, 6], :), 'LineWidth', 1.5)
+title('Cz')
+xlim([-200, 1000])
+subplot(3, 2, 4)
+plot(erp_times, pd_cpz([2, 4, 6], :), 'LineWidth', 1.5)
+title('CPz')
+xlim([-200, 1000])
+subplot(3, 2, 5)
+plot(erp_times, pd_pz([2, 4, 6], :), 'LineWidth', 1.5)
+title('Pz')
+xlim([-200, 1000])
+subplot(3, 2, 6)
+plot(erp_times, pd_poz([2, 4, 6], :), 'LineWidth', 1.5)
+title('POz')
+xlim([-200, 1000])
+legend({'00', '10', '11'})
+
+aa=bb
 % Plot effect size topos at selected time points for agency
 clim = [-0.3, 0.3];
-tpoints = [250, 500, 650];
+tpoints = [100, 280, 800];
 for t = 1 : length(tpoints)
     figure('Visible', 'off'); clf;
     tidx = erp_times >= tpoints(t) - 5 & erp_times <= tpoints(t) + 5;
@@ -451,7 +620,7 @@ end
 
 % Plot effect size topos at selected time points for difficulty
 clim = [-0.3, 0.3];
-tpoints = [0, 250, 450];
+tpoints = [0, 300, 500];
 for t = 1 : length(tpoints)
     figure('Visible', 'off'); clf;
     tidx = erp_times >= tpoints(t) - 5 & erp_times <= tpoints(t) + 5;
@@ -464,7 +633,7 @@ end
 
 % Plot effect size topos at selected time points for interaction
 clim = [-0.3, 0.3];
-tpoints = [250, 550, 650];
+tpoints = [300, 600, 700];
 for t = 1 : length(tpoints)
     figure('Visible', 'off'); clf;
     tidx = erp_times >= tpoints(t) - 5 & erp_times <= tpoints(t) + 5;
